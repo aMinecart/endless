e66132bebc99df26645cfdc53e60ff449fe390a6
aMinecart <ben.frankson@icloud.com>
2025-01-30 22:05:39 -0500

    Continued attempts to implement velocity-based acceleration

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,30 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveDir;
 
-    private Vector2 speed;
-    private float speedGoal = 1;
-    private float rampup;
+    private float speed = 3f;
+    private float acceleration = 1f;
+
+    private Vector2 accelerate(Vector2 currVel, Vector2 accelDir, float accelRate, float maxVel)
+    {
+        if (accelDir == Vector2.zero)
+        {
+            return currVel;
+        }
+        
+        float velRatio = (maxVel - currVel.magnitude) / maxVel;
+        float angleBetween = Vector2.Angle(currVel, accelDir);
+
+        //accelRate *= Mathf.Clamp01(velRatio + 0.5f) - angleBetween;
+
+        accelRate *= velRatio;
+
+        print(accelRate);
+
+        float accelMag = accelRate * maxVel * Time.fixedDeltaTime; // multiplying the acceleration by the max velocity makes it so that the player accelerates up to max velocity in the same amount of time, not matter how big it is
+        Vector2 newVel = currVel + accelMag * accelDir;
+
+        return Vector2.ClampMagnitude(newVel, maxVel);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -23,24 +44,14 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputValue moveValue)
     {
         moveDir = moveValue.Get<Vector2>();
+        //print(moveDir);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Mathf.Sign(rb.velocity.x) != Mathf.Sign(moveDir.x))
-        {
-            rampup = Mathf.MoveTowards(rampup, 1.0f, Time.fixedDeltaTime);
-        }
-        else if (moveDir.x == 0)
-        {
-            rampup = Mathf.MoveTowards(rampup, 1.0f, Time.fixedDeltaTime);
-        }
-        else
-        {
-            rampup = Mathf.MoveTowards(rampup, 30.0f, Time.fixedDeltaTime);
-        }
+        rb.velocity = accelerate(rb.velocity, moveDir, acceleration, speed);
 
-        rb.velocity = new Vector2(Mathf.MoveTowards(rb.velocity.x, speedGoal, rampup * Time.fixedDeltaTime), rb.velocity.y);
+        //print(rb.velocity.magnitude);
     }
 }
