12856a8ff33ba2ad514705435bfa6e774f727a64
aMinecart <ben.frankson@icloud.com>
2025-03-10 13:05:39 -0400

    Added phase

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -5,15 +5,14 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    private float dashTimer = 0.0f;
     [SerializeField] private Image dashCooldownImage;
     [SerializeField] private Button dashImage;
-    private PlayerMovement playerMovement;
+    private PlayerController playerController;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerMovement = GetComponentInParent<PlayerMovement>();
+        playerController = GetComponentInParent<PlayerController>();
         
         dashCooldownImage.fillAmount = 0.0f;
     }
@@ -21,6 +20,6 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        dashCooldownImage.fillAmount = playerMovement.dashUIFill;
+        dashCooldownImage.fillAmount = playerController.dashUIFill;
     }
 }
