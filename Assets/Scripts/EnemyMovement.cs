4af42355b0c1cb331e12a87f56180b3e33921e3c
aMinecart <ben.frankson@icloud.com>
2025-03-12 14:09:06 -0400

    Added variable to change where enemies start in their movement pattern
    Moved EnemyMovement.cs

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public float initial = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,6 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(0, 10 * Mathf.Cos(2 * Time.fixedTime));
+        rb.velocity = new Vector2(0, -10 * Mathf.Cos(2 * Time.fixedTime + initial));
     }
 }
